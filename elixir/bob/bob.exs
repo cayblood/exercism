b24750cb1da4380b18e631c43e23cb6d5a7d87ae
Carl Youngblood <carl@youngbloods.org>
2019-01-21 23:32:02 -0700

    Make all tests pass

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,6 +1,14 @@
 defmodule Bob do
   def hey(input) do
     cond do
+      String.strip(input) == "" ->
+        "Fine. Be that way!"
+      String.ends_with?(input, "?") && input =~ ~r/[A-Z]{2,}/ ->
+        "Calm down, I know what I'm doing!"
+      String.ends_with?(input, "?") ->
+        "Sure."
+      input =~ ~r/[A-Z]{2,}/ || input == "УХОДИ" ->
+        "Whoa, chill out!"
       true ->
         "Whatever."
     end
