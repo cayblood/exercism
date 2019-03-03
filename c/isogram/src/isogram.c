53b7dc12cd1421811eff143e89aa958998c9808e
Carl Youngblood <carl@youngbloods.org>
2019-03-03 10:16:09 -0700

    Remove memory allocation from isogram.c

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,21 +1,10 @@
 #include "isogram.h"
 #include <memory.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
 
 #define ALPHABET_SIZE 26
 #define ASCII_OFFSET 97
-
-void make_lowercase(char *dest, const char *src)
-{
-    int i = 0;
-    while (src[i]) {
-        dest[i] = (char)tolower(src[i]);
-        i++;
-    }
-}
 
 bool is_isogram(const char phrase[])
 {
@@ -26,16 +15,15 @@ bool is_isogram(const char phrase[])
         size_t length;
         length = strlen(phrase);
         memset(letters, 0, sizeof(int) * ALPHABET_SIZE);
-        char *lowercase = (char *)calloc(length, sizeof(char));
+        char letter;
         int index = 0;
 
         returnval = true;
-        make_lowercase(lowercase, phrase);
-
         for (size_t i = 0; i < length; ++i)
         {
-            index = lowercase[i] - ASCII_OFFSET;
-            if (index >= 0 && index < ALPHABET_SIZE)
+            letter = (char)tolower(phrase[i]);
+            index = letter - ASCII_OFFSET;
+            if (isalpha(letter))
             {
                 letters[index]++;
                 if (letters[index] > 1)
@@ -44,8 +32,6 @@ bool is_isogram(const char phrase[])
                 }
             }
         }
-
-        free(lowercase);
     }
     return returnval;
 }
