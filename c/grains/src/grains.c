de65f4f9e06414bc72d5841f8ac950f095ecd0ad
Carl Youngblood <carl@youngbloods.org>
2019-03-06 10:59:13 -0700

    Add grains exercise for C

---
+++ c/grains/src/grains.c
@@ -0,0 +1,26 @@
+//
+// Created by Carl Youngblood on 2019-03-06.
+//
+
+#include <math.h>
+#include <stdint.h>
+
+#define BOARD_SQUARE_COUNT 64
+
+uint64_t square(int n)
+{
+    uint64_t returnval = 0;
+    if (n > 0 && n <= BOARD_SQUARE_COUNT) {
+        returnval = (uint64_t)pow(2, (double)n - 1);
+    }
+    return returnval;
+}
+
+uint64_t total()
+{
+    uint64_t returnval = 0;
+    for (int i = 1; i <= BOARD_SQUARE_COUNT; i++) {
+        returnval += square(i);
+    }
+    return returnval;
+}
---
+++ c/grains/src/grains.h
@@ -0,0 +1,13 @@
+//
+// Created by Carl Youngblood on 2019-03-06.
+//
+
+#ifndef GRAINS_GRAINS_H
+#define GRAINS_GRAINS_H
+
+#include <stdint.h>
+
+uint64_t square(int n);
+uint64_t total();
+
+#endif //GRAINS_GRAINS_H
