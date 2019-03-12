1d7ec5b2fdf2b18272e926435637a94dba93b132
Carl Youngblood <carl@youngbloods.org>
2019-03-11 23:29:39 -0600

    First failing test

---
+++ c/meetup/src/meetup.c
@@ -0,0 +1,8 @@
+//
+// Created by Carl Youngblood on 2019-03-11.
+//
+
+int meetup_day_of_month(int year, int month, const char ordinal[], const char day_of_week[])
+{
+    return 0;
+}
---
+++ c/meetup/src/meetup.h
@@ -0,0 +1,10 @@
+//
+// Created by Carl Youngblood on 2019-03-11.
+//
+
+#ifndef MEETUP_MEETUP_H
+#define MEETUP_MEETUP_H
+
+int meetup_day_of_month(int year, int month, const char ordinal[], const char day_of_week[]);
+
+#endif //MEETUP_MEETUP_H
