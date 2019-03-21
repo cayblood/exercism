b355feea8a6f5676efee73172379a1c9951a3b8b
Carl Youngblood <carl@youngbloods.org>
2019-03-20 23:58:22 -0400

    WIP

---
+++ c/beer-song/src/beer_song.c
@@ -0,0 +1,11 @@
+//
+// Created by Youngblood, Carl on 2019-03-20.
+//
+
+void verse(char *response, int beer_count)
+{
+    const char phrase1[] = "%d bottle%s of beer";
+    const char phrase2[] = "Take one down and pass it around";
+    const char phrase3[] = "Go to the store and buy some more";
+    const char template[] = "%s on the wall, %s.\n%s, %s on the wall.";
+}
---
+++ c/beer-song/src/beer_song.h
@@ -0,0 +1,10 @@
+//
+// Created by Youngblood, Carl on 2019-03-20.
+//
+
+#ifndef BEERSONG_BEER_SONG_H
+#define BEERSONG_BEER_SONG_H
+
+void verse(char *response, int beer_count);
+
+#endif //BEERSONG_BEER_SONG_H
