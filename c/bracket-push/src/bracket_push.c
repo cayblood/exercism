def80c9fa67c5a707f6d73687f31c3004d094fa6
Carl Youngblood <carl@youngbloods.org>
2019-03-30 23:05:38 -0600

    First test passing for c/bracket_push

---
+++ c/bracket-push/src/bracket_push.c
@@ -0,0 +1,13 @@
+//
+// Created by Carl Youngblood on 2019-03-30.
+//
+
+#include "bracket_push.h"
+
+#include <string.h>
+
+bool is_paired(const char *input)
+{
+    strlen(input);
+    return true;
+}
---
+++ c/bracket-push/src/bracket_push.h
@@ -0,0 +1,12 @@
+//
+// Created by Carl Youngblood on 2019-03-30.
+//
+
+#ifndef BRACKET_PUSH_H
+#define BRACKET_PUSH_H
+
+#include <stdbool.h>
+
+bool is_paired(const char *input);
+
+#endif //BRACKET_PUSH_H
