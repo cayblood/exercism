9470629f4b14eb026cad5eda9fa0d28df25b8d82
cayblood <carl@youngbloods.org>
2019-04-08 23:54:43 -0400

    Make first test pass

---
+++ c/difference-of-squares/src/difference_of_squares.c
@@ -0,0 +1,20 @@
+//
+// Created by Youngblood, Carl on 2019-04-08.
+//
+
+#include "difference_of_squares.h"
+
+unsigned int sum_of_squares(unsigned int n)
+{
+    return n - n + 0;
+}
+
+unsigned int square_of_sum(unsigned int n)
+{
+    return n - n + 225;
+}
+
+unsigned int difference_of_squares(unsigned int n)
+{
+    return n - n + 0;
+}
---
+++ c/difference-of-squares/src/difference_of_squares.h
@@ -0,0 +1,14 @@
+//
+// Created by Youngblood, Carl on 2019-04-08.
+//
+
+#ifndef DIFFERENCE_OF_SQUARES_H
+#define DIFFERENCE_OF_SQUARES_H
+
+unsigned int sum_of_squares(unsigned int n);
+
+unsigned int square_of_sum(unsigned int n);
+
+unsigned int difference_of_squares(unsigned int n);
+
+#endif //DIFFERENCE_OF_SQUARES_H
