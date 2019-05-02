d56e93971c8ed040a75ecd414c63fa36c02d8cc5
cayblood <carl@youngbloods.org>
2019-05-02 00:01:06 -0700

    c/raindrops: initial commit

---
+++ c/raindrops/src/raindrops.c
@@ -0,0 +1,10 @@
+//
+// Created by Youngblood, Carl on 2019-05-01.
+//
+
+#include "raindrops.h"
+
+void convert(char *result, int drops)
+{
+    
+}
---
+++ c/raindrops/src/raindrops.h
@@ -0,0 +1,10 @@
+//
+// Created by Youngblood, Carl on 2019-05-01.
+//
+
+#ifndef RAINDROPS_H
+#define RAINDROPS_H
+
+void convert(char *result, int drops);
+
+#endif //RAINDROPS_H
