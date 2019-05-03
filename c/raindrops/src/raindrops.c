4bb6b212bdc312675b33d8f195f6d9b84ad604b9
cayblood <carl@youngbloods.org>
2019-05-02 23:11:12 -0700

    c/raindrops: all test passing

--- c/raindrops/src/raindrops.c
+++ c/raindrops/src/raindrops.c
@@ -4,8 +4,31 @@
 
 #include "raindrops.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_STRING_LENGTH 256
 
 void convert(char *result, int drops)
 {
-    sprintf(result, "%d", drops);
+    bool has_factor = false;
+    char tempstr[MAX_STRING_LENGTH] = "";
+
+    if (drops % 3 == 0) {
+        strcat(tempstr, "Pling");
+        has_factor = true;
+    }
+    if (drops % 5 == 0) {
+        strcat(tempstr, "Plang");
+        has_factor = true;
+    }
+    if (drops % 7 == 0) {
+        strcat(tempstr, "Plong");
+        has_factor = true;
+    }
+    if (has_factor) {
+        strcpy(result, tempstr);
+    } else {
+        sprintf(result, "%d", drops);
+    }
 }
