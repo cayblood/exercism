4426a50a6095c6531a0335e3ba1cd1628fe2adc3
Carl Youngblood <carl@youngbloods.org>
2019-05-14 23:50:53 -0400

    First test passing

---
+++ c/binary-search/src/binary_search.c
@@ -0,0 +1,13 @@
+//
+// Created by Carl Youngblood on 2019-05-14.
+//
+
+#include "binary_search.h"
+
+int *binary_search(int to_find, int arr[], int length)
+{
+    if (to_find && length > 0) {
+        return &arr[0];
+    }
+    return &arr[0];
+}
---
+++ c/binary-search/src/binary_search.h
@@ -0,0 +1,10 @@
+//
+// Created by Carl Youngblood on 2019-05-14.
+//
+
+#ifndef BINARY_SEARCH_H
+#define BINARY_SEARCH_H
+
+int *binary_search(int to_find, int arr[], int length);
+
+#endif // BINARY_SEARCH_H
