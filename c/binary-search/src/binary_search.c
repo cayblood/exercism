815a9eacda7236eb9e4eb0c8ac2d8c164e7460d7
Carl Youngblood <carl@youngbloods.org>
2019-05-19 09:24:17 -0600

    c/binary-search: all tests passing

--- c/binary-search/src/binary_search.c
+++ c/binary-search/src/binary_search.c
@@ -7,25 +7,30 @@
 
 int *binary_search(int to_find, int arr[], int length)
 {
-    int *returnval = NULL;
+    int *to_return = NULL;
     int max = length - 1;
     int min = 0;
     int index = max - (length / 2);
 
     if (to_find && length > 0) {
-        while (arr[index] != to_find) {
-            if (arr[index] > to_find) {
+        int current_val = arr[index];
+
+        while (current_val != to_find) {
+            if (current_val > to_find) {
                 max = index - 1;
             } else {
                 min = index + 1;
             }
+            length = max - min;
+            index = max - (length / 2);
+            current_val = arr[index];
             if (length < 1) {
                 break;
             }
-            length = max - min;
-            index = max - (length / 2);
         }
-        returnval = &arr[index];
+        if (current_val == to_find) {
+            to_return = &arr[index];
+        }
     }
-    return returnval;
+    return to_return;
 }
--- c/binary-search/test/test_binary_search.c
+++ c/binary-search/test/test_binary_search.c
@@ -32,7 +32,6 @@ void test_value_at_beginning(void)
 
 void test_value_at_end(void)
 {
-   TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
    TEST_ASSERT(&arr[6] == binary_search(11, arr, length));
@@ -40,7 +39,6 @@ void test_value_at_end(void)
 
 void test_find_value_with_odd_length(void)
 {
-   TEST_IGNORE();
    int arr[] = { 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
    TEST_ASSERT(&arr[9] == binary_search(144, arr, length));
@@ -48,7 +46,6 @@ void test_find_value_with_odd_length(void)
 
 void test_find_value_with_even_length(void)
 {
-   TEST_IGNORE();
    int arr[] = { 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
    TEST_ASSERT(&arr[5] == binary_search(21, arr, length));
@@ -56,7 +53,6 @@ void test_find_value_with_even_length(void)
 
 void test_identify_missing_value(void)
 {
-   TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
    TEST_ASSERT(NULL == binary_search(7, arr, length));
@@ -64,7 +60,6 @@ void test_identify_missing_value(void)
 
 void test_value_smaller_than_everything(void)
 {
-   TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
    TEST_ASSERT(NULL == binary_search(0, arr, length));
@@ -72,7 +67,6 @@ void test_value_smaller_than_everything(void)
 
 void test_value_larger_than_everything(void)
 {
-   TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
    TEST_ASSERT(NULL == binary_search(13, arr, length));
@@ -80,7 +74,6 @@ void test_value_larger_than_everything(void)
 
 void test_empty_array(void)
 {
-   TEST_IGNORE();
    int *arr = NULL;
    size_t length = 0;
    TEST_ASSERT(NULL == binary_search(1, arr, length));
@@ -88,7 +81,6 @@ void test_empty_array(void)
 
 void test_zero_length_array(void)
 {
-   TEST_IGNORE();
    int arr[] = { 1 };
    size_t length = 0;
    TEST_ASSERT(NULL == binary_search(1, arr, length));
