5cb075180d4e1bd50f320dca4e4a120c1e39f7b5
Carl Youngblood <carl@youngbloods.org>
2019-05-20 22:49:48 -0600

    c/sum_of_multiples: first test passing

---
+++ c/sum-of-multiples/src/sum_of_multiples.c
@@ -0,0 +1,10 @@
+//
+// Created by Carl Youngblood on 2019-05-20.
+//
+
+#include "sum_of_multiples.h"
+
+int sum_of_multiples(const unsigned int multiples, int count, int max)
+{
+    return 0;
+}
---
+++ c/sum-of-multiples/src/sum_of_multiples.h
@@ -0,0 +1,10 @@
+//
+// Created by Carl Youngblood on 2019-05-20.
+//
+
+#ifndef SUM_OF_MULTIPLES_H
+#define SUM_OF_MULTIPLES_H
+
+int sum_of_multiples(const unsigned int multiples, int count, int max);
+
+#endif //SUM_OF_MULTIPLES_H
