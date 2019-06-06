e825b0499c4f7c9c736961eb5e024d1f4cf260e7
Carl Youngblood <carl@youngbloods.org>
2019-06-05 21:09:33 -0600

    c/complex-numbers: first test passing

--- c/complex-numbers/src/complex_numbers.c
+++ c/complex-numbers/src/complex_numbers.c
@@ -2,45 +2,58 @@
 
 complex_t c_add(complex_t a, complex_t b)
 {
-   // TODO: implement
+    complex_t to_return;
+    return to_return;
 }
 
 complex_t c_sub(complex_t a, complex_t b)
 {
-   // TODO: implement
+    complex_t to_return;
+    return to_return;
 }
 
 complex_t c_mul(complex_t a, complex_t b)
 {
-   // TODO: implement
+    complex_t to_return;
+
+    to_return.real = (a.real * b.real) - (a.imag * b.imag);
+    to_return.imag = (a.real * b.imag) + (a.imag * b.real);
+
+    return to_return;
 }
 
 complex_t c_div(complex_t a, complex_t b)
 {
-   // TODO: implement
+    complex_t to_return;
+    return to_return;
 }
 
 double c_abs(complex_t x)
 {
-   // TODO: implement
+    double to_return = 0.0;
+    return to_return;
 }
 
 complex_t c_conjugate(complex_t x)
 {
-   // TODO: implement
+    complex_t to_return;
+    return to_return;
 }
 
 double c_real(complex_t x)
 {
-   // TODO: implement
+    double to_return = 0.0;
+    return to_return;
 }
 
 double c_imag(complex_t x)
 {
-   // TODO: implement
+    double to_return = 0.0;
+    return to_return;
 }
 
 complex_t c_exp(complex_t x)
 {
-   // TODO: implement
+    complex_t to_return;
+    return to_return;
 }
